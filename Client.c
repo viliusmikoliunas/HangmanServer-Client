c33b3834aff7646613994b951cfda050fa078a99
viliusmikoliunas <vilius.mikoliunas@gmail.com>
2017-03-30 22:53:32 +0300

    Client now can request stats of the specific user
    Input of specific username is not implemented yet (fgets bugs)

--- Client.c
+++ Client.c
@@ -5,6 +5,8 @@
 static char userQuitChar = '0';
 static char userStartChar = '1';
 static char userStatsChar = '2';
+static char specificUserStatsChar = '3';
+static char allUserStatsChar = '4';
 
 void SendUsername(int socket, char* username)
 {//U:|strlen(username)|username
@@ -21,12 +23,18 @@ void SendUsername(int socket, char* username)
 	strcat(msgToSend,username);
 	strcat(msgToSend,"\0");
 	write(socket,msgToSend,strlen(msgToSend));
+	
+	free(usernameLength);
+	free(msgToSend);
+	
 }
 void PrintMenu()
 {	
 	printf("%c.Quit\n",userQuitChar);
 	printf("%c.Start\n",userStartChar);
-	printf("%c.Stats\n",userStatsChar);
+	printf("%c.Personal Stats\n",userStatsChar);
+	printf("%c.Specific user Stats\n",specificUserStatsChar);
+	printf("%c.All Stats\n",allUserStatsChar);
 }
 int SendGameMove(int socket,char* move)
 {
@@ -72,6 +80,7 @@ void DisplayStats(char* buffer)
 		l++;
 		i++;
 	}
+	*(wins+l) = '\0';
 	i++;
 	l=0;
 	while(*(buffer+i)!='\0')
@@ -80,6 +89,7 @@ void DisplayStats(char* buffer)
 		l++;
 		i++;
 	}
+	*(losses+l) = '\0';
 	printf("Wins:%s\nLosses:%s\n",wins,losses);
 	free(losses);
 	free(wins);
@@ -94,14 +104,14 @@ int main(int argc, char *argv[]){
     char sendbuffer[BUFFLEN];
 
     int i;
-
+/*
     if (argc != 3){
         fprintf(stderr,"USAGE: %s <ip> <port>\n",argv[0]);
         exit(1);
-    }
+    }*/
 
-    port = atoi(argv[2]);
-	//port = atoi("7896");
+    //port = atoi(argv[2]);
+	port = atoi("7896");
 	
     if ((port < 1) || (port > 65535)){
         printf("ERROR #1: invalid port specified.\n");
@@ -118,8 +128,8 @@ int main(int argc, char *argv[]){
     servaddr.sin_port = htons(port); // nurodomas portas
     
 	 
-    if ( inet_aton(argv[1], &servaddr.sin_addr) <= 0 ) {
-    //if ( inet_aton("127.0.0.1", &servaddr.sin_addr) <= 0 ) {
+    //if ( inet_aton(argv[1], &servaddr.sin_addr) <= 0 ) {
+    if ( inet_aton("127.0.0.1", &servaddr.sin_addr) <= 0 ) {
         fprintf(stderr,"ERROR #3: Invalid remote IP address.\n");
         exit(1);
     }
@@ -132,11 +142,10 @@ int main(int argc, char *argv[]){
         fprintf(stderr,"ERROR #4: error in connect().\n");
         exit(1);
     }
-	else 
-	{
-		puts("Welcome to Hangman");
-		PrintMenu();
-	}
+	
+	puts("Welcome to Hangman");
+	PrintMenu();
+	
     memset(&sendbuffer,0,BUFFLEN);
     fcntl(0,F_SETFL,fcntl(0,F_GETFL,0)|O_NONBLOCK);
 	
@@ -150,12 +159,6 @@ int main(int argc, char *argv[]){
 
         select(s_socket+1,&read_set,NULL,NULL,NULL);
 
-		if(!usernameSent)
-		{
-			SendUsername(s_socket,username);
-			usernameSent = true;
-		}
-		
         if (FD_ISSET(s_socket, &read_set))//gavimas
 		{
             memset(&recvbuffer,0,BUFFLEN);
@@ -166,7 +169,8 @@ int main(int argc, char *argv[]){
             }
 			else if(strstr(recvbuffer,statisticsHandle)!=NULL)
 			{
-				DisplayStats(recvbuffer);
+				if(recvbuffer[2]=='0') puts("No records of user");
+				else DisplayStats(recvbuffer);
 			}
 			
 			else if(strstr(recvbuffer,gameWonHandle)!=NULL)
@@ -184,9 +188,16 @@ int main(int argc, char *argv[]){
 			else printf("%s\n",recvbuffer);
         }
 		
+		else if(FD_ISSET(0,&read_set)&&!usernameSent)
+			{
+				SendUsername(s_socket,username);
+				usernameSent = true;
+			}
+			
         else if (FD_ISSET(0,&read_set))//siuntimas
 		{
 			fgets(sendbuffer,BUFFLEN,stdin);
+			
 			if(sendbuffer[0]==userQuitChar) //users wants to quit
 			{
 				write(s_socket, quitHandle,strlen(quitHandle));
@@ -206,12 +217,30 @@ int main(int argc, char *argv[]){
 				{
 					gameUnderway = true;
 				}
-				else if (sendbuffer[0] == userStatsChar)//user wants statistics
+				else if (sendbuffer[0] == userStatsChar)//user wants personal statistics
 				{
 					write(s_socket, statHandle,strlen(statHandle));
 				}
+				else if (sendbuffer[0] == specificUserStatsChar)
+				{
+					char* tempUsername = "E:pokemonas";
+					write(s_socket,tempUsername,strlen(tempUsername));
+					/*
+					char* otherUsername = malloc(20);
+					printf("User name of the player:\n");
+					while ( getchar() != '\n');
+					fgets(otherUsername,20,stdin);
+					char* msgToSend = malloc(40);
+					strcpy(msgToSend,specificUserStatsHandle);
+					strcat(msgToSend,otherUsername);
+					strcat(msgToSend,"\0");
+					write(s_socket,msgToSend,strlen(msgToSend));
+					free(otherUsername);
+					free(msgToSend);
+					*/
+				}
 			}		
-        }
+		}
     }
 	puts("You have been disconnected");
     close(s_socket);
